3f0663c85553a57f0463619eaad6ca30d8d292fb
Andrew Taylor <contact@ajtaylor.dev>
2024-10-06 18:55:22 +0100

    Changes to Nav buttons

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -15,7 +15,6 @@ background-color: #f6f6f6;
     margin: 0px;
     padding: 0px 20px 0px 20px;
     display: inline-block;
-    line-height: 40px;
     color: #333;
     background-color: #c3c3c3;
     cursor: pointer;
@@ -23,6 +22,12 @@ background-color: #f6f6f6;
 & li:hover {
     background-color: #e4e4e4;
 }
+
+& a.nav_link {
+    diplay: block;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
 }
 `
 
@@ -35,7 +40,7 @@ export default function Navigation() {
     return (
         <SCS_NavBar>
             {navbuttons.map((items) => (
-                <li key={items.value}>{items.value}</li>
+                <li key={items.value}><a className="nav_link" href="#">{items.value}</a></li>
             ))}
         </SCS_NavBar>
     )
